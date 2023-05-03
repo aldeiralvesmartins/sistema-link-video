146e9d07204970ea9ac4c6ef44489a33d166035c
aldeir <95594532+aldeiralvesmartins@users.noreply.github.com>
2023-05-03 11:09:59 -0300

    Update tabela-usuario.php

--- tabela-usuario.php
+++ tabela-usuario.php
@@ -4,3 +4,7 @@ $dbPath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO("sqlite:$dbPath");
 
 $pdo->exec('ALTER TABLE videos ADD COLUMN image_path TXT');
+
+
+
+como resolver esse problema ,Your requirements could not be resolved to an installable set of packages. Problem 1 - Root composer.json requires php ^8.1 but your php version (7.2.34) does not satisfy that requirement. Problem 2 - Root composer.json requires laravel/sanctum ^3.2 -> satisfiable by laravel/sanctum[v3.2.0, ..., v3.2.5]. - laravel/sanctum[v3.2.0, ..., v3.2.5] require php ^8.0.2 -> your php version (7.2.34) does not satisfy that requirement. Problem 3 - laravel/framework[v10.8.0, ..., v10.9.0] require php ^8.1 -> your php version (7.2.34) does not satisfy that requirement. - Root composer.json requires laravel/framework ^10.8 -> satisfiable by laravel/framework[v10.8.0, v10.9.0].
